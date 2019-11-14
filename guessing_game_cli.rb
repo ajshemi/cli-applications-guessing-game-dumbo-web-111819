cb4f979486203783f5ecc915b58dfadc01f5cfe5
essential-hacker-4489 <akpojotor@yahoo.com>
2019-11-14 02:47:26 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   num=1+rand(5)
   puts "guess their own number between 1 and 6"
-  input=get.chomp
+  input=gets.chomp
   if num == input
     puts "You guessed the correct number!"
   elsif num == "exit"
